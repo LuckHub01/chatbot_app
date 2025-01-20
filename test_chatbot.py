958ddbfc5e3fde15e5676959fddf08995c8f51b2
LuckHub01 <inaparehub@gmail.com>
2025-01-20 04:01:11 +0000

    ajout des données

--- test_chatbot.py
+++ test_chatbot.py
@@ -32,7 +32,8 @@ def detect_language(text):
 from googletrans import Translator
 
 
-logo = r"data\images\genai1.png"  # Remplacez par le chemin de votre image
+logo = "data/images/genai1.png"
+  # Remplacez par le chemin de votre image
 
 st.logo(logo, icon_image=logo)
 
@@ -319,7 +320,7 @@ def get_pdfs_from_folder(folder_path):
                 pdf_files.append(os.path.join(folder_path, file_name))
         return pdf_files
     
-folder_path = r"data\data"
+folder_path = "data/data"
 
 # Charger le modèle de résumé
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
@@ -362,7 +363,8 @@ def main():
     st.text("Maya, votre protectrice digitale")
     
     # Ajouter une image à gauche dans la barre latérale
-    sidebar_image_path = r"data\images\im1.jpg"  # Remplacez par le chemin de votre image
+    sidebar_image_path =  "data/images/im1.jpg"
+ # Remplacez par le chemin de votre image
     if os.path.exists(sidebar_image_path):
         # sidebar_image = Image.open(sidebar_image_path)
         # st.sidebar.image(sidebar_image, use_column_width=True)
@@ -379,7 +381,8 @@ def main():
             """,
             unsafe_allow_html=True
         ) # Ajouter une image au centre avant le titre
-    center_image_path = r"data\images\im1bg.png"  # Remplacez par le chemin de votre image
+    center_image_path = "data/images/im1bg.png"
+ # Remplacez par le chemin de votre image
    
     if os.path.exists(center_image_path):
         # Charger l'image
