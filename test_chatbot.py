f15527901c09d21e5229e465a686b1c5ec85f031
LuckHub01 <150858694+LuckHub01@users.noreply.github.com>
2026-04-30 14:01:40 +0000

    Choix bon modele gemini 2.5

--- test_chatbot.py
+++ test_chatbot.py
@@ -26,7 +26,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 print([m.name for m in genai.list_models()])
 
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 
